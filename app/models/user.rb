4d03bfa1dea3ca54ecd59de21e1d58cc9b3d61c4
wakatakatakataka <wakatakatakataka@gmail.com>
2021-04-08 14:35:13 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :products, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+  
+end
