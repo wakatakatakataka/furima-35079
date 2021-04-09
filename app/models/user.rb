f05ad6a3a1bf1f5078ebd1cfd98a04b793163acf
wakatakatakataka <wakatakatakataka@gmail.com>
2021-04-09 11:18:32 +0900

    テストコード記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,16 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX , message: "Include both letters and numbers"
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: 'Full-width characters' } do
+    validates :first_name
+    validates :family_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: 'Full-width katakana characters'} do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
   validates :birth_day, presence: true
   
 end
